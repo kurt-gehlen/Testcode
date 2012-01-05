4e6102ecc67ca695cd8000b71193c15f5440129b
Kurt Gehlen <kurtgehlen@yahoo.com>
2012-01-05 10:10:32 -0800

    Added done line

--- sizeof.cpp
+++ sizeof.cpp
@@ -5,4 +5,5 @@ main(int argc,char ** argv)
 {
 	printf("int = %d, long = %d, long long = %d\n", sizeof(int), sizeof(long), sizeof(long long));
 	printf("int * = %d, void * = %d\n", sizeof(int *),sizeof(void*));
+	printf("done\n");
 }
